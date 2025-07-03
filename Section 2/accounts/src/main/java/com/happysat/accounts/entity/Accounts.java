1b243c5839077f2bc8a3ae2972cf44a1ee2892aa
anonymous-lad2 <anonymouslad540@gmail.com>
2025-07-03 18:27:34 +0530

    entities created

---
+++ Section 2/accounts/src/main/java/com/happysat/accounts/entity/Accounts.java
@@ -0,0 +1,26 @@
+package com.happysat.accounts.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter @Setter @ToString @NoArgsConstructor @AllArgsConstructor
+public class Accounts {
+
+    @Column(name = "customer_id")
+    private Long customerId;
+
+    @Column(name = "account_number")
+    @Id
+    private Long accountNumber;
+
+    @Column(name = "account_type")
+    private String accountType;
+
+    @Column(name = "branch_address")
+    private String branchAddress;
+
+
+}
