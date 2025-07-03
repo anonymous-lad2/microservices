c5929d8377b16fd3ad10f49ad085ea6ed1eb961d
anonymous-lad2 <anonymouslad540@gmail.com>
2025-07-03 18:48:44 +0530

    repository and dto cerated

---
+++ Section 2/accounts/src/main/java/com/happysat/accounts/repository/AccountRepository.java
@@ -0,0 +1,9 @@
+package com.happysat.accounts.repository;
+
+import com.happysat.accounts.entity.Accounts;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Accounts, Long> {
+}
---
+++ Section 2/accounts/src/main/java/com/happysat/accounts/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.happysat.accounts.repository;
+
+import com.happysat.accounts.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
