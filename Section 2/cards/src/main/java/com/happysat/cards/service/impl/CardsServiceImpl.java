d545cbdf2e7bb3a05c098e523068c1b61e743806
anonymous-lad2 <anonymouslad540@gmail.com>
2025-07-07 00:39:39 +0530

    exceptions added

--- Section 2/cards/src/main/java/com/happysat/cards/service/impl/CardsServiceImpl.java
+++ Section 2/cards/src/main/java/com/happysat/cards/service/impl/CardsServiceImpl.java
@@ -1,13 +1,23 @@
 package com.happysat.cards.service.impl;
 
 import com.happysat.cards.dto.CardsDto;
+import com.happysat.cards.entity.Cards;
+import com.happysat.cards.repository.CardRepository;
 import com.happysat.cards.service.CardsService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
 public class CardsServiceImpl implements CardsService {
 
+    private CardRepository cardRepository;
 
     @Override
     public void createCard(String mobileNumber) {
+        Optional<Cards> opt = cardRepository.findByMobileNumber(mobileNumber);
 
     }
 
