44641bd3c2d7a05b7e6a4a0c31769b570cbfb7e6
anonymous-lad2 <anonymouslad540@gmail.com>
2025-07-07 00:24:38 +0530

    card service implementation

---
+++ Section 2/cards/src/main/java/com/happysat/cards/repository/CardRepository.java
@@ -0,0 +1,9 @@
+package com.happysat.cards.repository;
+
+import com.happysat.cards.entity.Cards;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CardRepository extends JpaRepository<Cards, Long> {
+}
---
+++ Section 2/cards/src/main/java/com/happysat/cards/service/impl/CardsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.happysat.cards.service.impl;
+
+import com.happysat.cards.dto.CardsDto;
+import com.happysat.cards.service.CardsService;
+
+public class CardsServiceImpl implements CardsService {
+
+
+    @Override
+    public void createCard(String mobileNumber) {
+
+    }
+
+    @Override
+    public CardsDto fetchCard(String mobileNumber) {
+        return null;
+    }
+
+    @Override
+    public boolean updateCard(CardsDto cardsDto) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteCard(String mobileNumber) {
+        return false;
+    }
+}
