b66b98aac514f323439a378e15cf9bf0c69df1d2
anonymous-lad2 <anonymouslad540@gmail.com>
2025-07-07 00:57:12 +0530

    cards service implemented

--- Section 2/cards/src/main/java/com/happysat/cards/service/impl/CardsServiceImpl.java
+++ Section 2/cards/src/main/java/com/happysat/cards/service/impl/CardsServiceImpl.java
@@ -1,13 +1,18 @@
 package com.happysat.cards.service.impl;
 
+import com.happysat.cards.constants.CardsConstants;
 import com.happysat.cards.dto.CardsDto;
 import com.happysat.cards.entity.Cards;
+import com.happysat.cards.exception.CardAlreadyExistsException;
+import com.happysat.cards.exception.ResourceNotFoundException;
+import com.happysat.cards.mapper.CardsMapper;
 import com.happysat.cards.repository.CardRepository;
 import com.happysat.cards.service.CardsService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.Random;
 
 @Service
 @AllArgsConstructor
@@ -18,21 +23,50 @@ public class CardsServiceImpl implements CardsService {
     @Override
     public void createCard(String mobileNumber) {
         Optional<Cards> opt = cardRepository.findByMobileNumber(mobileNumber);
-
+        if(opt.isPresent()){
+            throw new CardAlreadyExistsException("Card already registered with given mobileNumber "+mobileNumber);
+        }
+        cardRepository.save(createNewCard(mobileNumber));
     }
+
+    private Cards createNewCard(String mobileNumber) {
+        Cards newCard = new Cards();
+        long randomCardNumber = 100000000000L + new Random().nextInt(900000000);
+        newCard.setCardNumber(Long.toString(randomCardNumber));
+        newCard.setMobileNumber(mobileNumber);
+        newCard.setCardType(CardsConstants.CREDIT_CARD);
+        newCard.setTotalLimit(CardsConstants.NEW_CARD_LIMIT);
+        newCard.setAmountUsed(0);
+        newCard.setAvailableAmount(CardsConstants.NEW_CARD_LIMIT);
+        return newCard;
+    }
+
 
     @Override
     public CardsDto fetchCard(String mobileNumber) {
-        return null;
+        Cards cards = cardRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Card", "mobileNumber", mobileNumber)
+        );
+        return CardsMapper.mapToCardsDto(cards, new CardsDto());
     }
 
     @Override
     public boolean updateCard(CardsDto cardsDto) {
-        return false;
+        Cards cards = cardRepository.findByCardNumber(cardsDto.getCardNumber()).orElseThrow(
+                () -> new ResourceNotFoundException("Card", "CardNumber", cardsDto.getCardNumber())
+        );
+        CardsMapper.mapToCards(cardsDto, cards);
+        cardRepository.save(cards);
+        return true;
     }
 
     @Override
     public boolean deleteCard(String mobileNumber) {
-        return false;
+        Cards cards = cardRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Card", "mobileNumber", mobileNumber)
+        );
+        cardRepository.deleteById(cards.getCardId());
+        return true;
     }
+
 }
