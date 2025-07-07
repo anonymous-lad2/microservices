c6a4e0838016f2bc0a9ba8173e11ade1b8e1863f
anonymous-lad2 <anonymouslad540@gmail.com>
2025-07-08 01:25:15 +0530

    improved entity

--- Section 2/loans/src/main/java/com/happysat/loans/entity/Loans.java
+++ Section 2/loans/src/main/java/com/happysat/loans/entity/Loans.java
@@ -5,7 +5,7 @@ import lombok.*;
 
 @Entity
 @Getter @Setter @ToString @AllArgsConstructor @NoArgsConstructor
-public class Loans {
+public class Loans extends BaseEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
